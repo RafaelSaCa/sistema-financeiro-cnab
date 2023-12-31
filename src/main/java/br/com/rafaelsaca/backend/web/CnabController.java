da454b6f35453c2a6cf12280a964268bbe73fdca
Rafael Saca <raffaellsaca@gmail.com>
2023-12-31 12:14:46 -0300

    feat: Controller para chamada do service

--- src/main/java/br/com/rafaelsaca/backend/web/CnabController.java
+++ src/main/java/br/com/rafaelsaca/backend/web/CnabController.java
@@ -6,13 +6,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import br.com.rafaelsaca.backend.domain.CnabService;
+
 @RestController
 @RequestMapping("cnab")
 public class CnabController {
+    private final CnabService service;
+
+    public CnabController(CnabService service) {
+        this.service = service;
+    }
 
     @PostMapping("upload")
-    public String upload(@RequestParam("file") MultipartFile file) {
+    public String upload(@RequestParam("file") MultipartFile file) throws Exception {
+        service.uploadCnabFile(file);
         return "Processamento iniciado !";
-
     }
 }
