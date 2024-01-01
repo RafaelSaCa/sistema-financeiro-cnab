94c3d76ffca4267d0d8f8ebf01cf64b536b5628f
Rafael Saca <raffaellsaca@gmail.com>
2024-01-01 15:41:50 -0300

    feat: Anotations para mapear Id e Coluna no banco de dados

--- src/main/java/br/com/rafaelsaca/backend/entity/Transacao.java
+++ src/main/java/br/com/rafaelsaca/backend/entity/Transacao.java
@@ -6,34 +6,37 @@ import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+
 public record Transacao(
-                Long id,
-                Integer tipo,
-                Date data,
-                BigDecimal valor,
-                Long cpf,
-                String cartao,
-                Time hora,
-                String donoDaLoja,
-                String nomeDaLoja) {
+    @Id Long id,
+    Integer tipo,
+    Date data,
+    BigDecimal valor,
+    Long cpf,
+    String cartao,
+    Time hora,
+    @Column("DONO_LOJA") String donoDaLoja,
+    @Column("NOME_LOJA") String nomeDaLoja) {
 
-        public Transacao withValor(BigDecimal valor) {
-                return new Transacao(id, tipo, data, valor, cpf, cartao, hora, donoDaLoja, nomeDaLoja);
-        }
+  public Transacao withValor(BigDecimal valor) {
+    return new Transacao(id, tipo, data, valor, cpf, cartao, hora, donoDaLoja, nomeDaLoja);
+  }
 
-        public Transacao withData(String data) throws ParseException {
-                var dateFormat = new SimpleDateFormat("yyyyMMdd");
-                var date = dateFormat.parse(data);
+  public Transacao withData(String data) throws ParseException {
+    var dateFormat = new SimpleDateFormat("yyyyMMdd");
+    var date = dateFormat.parse(data);
 
-                return new Transacao(id, tipo, new Date(date.getTime()), valor, cpf, cartao, hora, donoDaLoja,
-                                nomeDaLoja);
-        }
+    return new Transacao(id, tipo, new Date(date.getTime()), valor, cpf, cartao, hora, donoDaLoja,
+        nomeDaLoja);
+  }
 
-        public Transacao withHora(String hora) throws ParseException {
-                var dateFormat = new SimpleDateFormat("HHmmss");
-                var date = dateFormat.parse(hora);
+  public Transacao withHora(String hora) throws ParseException {
+    var dateFormat = new SimpleDateFormat("HHmmss");
+    var date = dateFormat.parse(hora);
 
-                return new Transacao(id, tipo, data, valor, cpf, cartao, new Time(date.getTime()), donoDaLoja,
-                                nomeDaLoja);
-        }
+    return new Transacao(id, tipo, data, valor, cpf, cartao, new Time(date.getTime()), donoDaLoja,
+        nomeDaLoja);
+  }
 }
